aec74bd22926bed94bb2fcaec886a3246e6639f1
Feaxmutch <feaxmutch@gmail.com>
2024-10-13 12:33:27 +0300

    Copyed the old solution

--- RepeatestNumber/Program.cs
+++ RepeatestNumber/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int minNumber = 0;
+            int maxNumber = 2;
+            int[] numbers = new int[30];
+            int repeatsCount = 1;
+            int maxRepeatsCount = 1;
+            int repeatestNumber = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(minNumber, maxNumber + 1);
+                Console.Write($"{numbers[i]} ");
+            }
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    repeatsCount++;
+                }
+                else
+                {
+                    repeatsCount = 1;
+                }
+
+                if (repeatsCount > maxRepeatsCount)
+                {
+                    maxRepeatsCount = repeatsCount;
+                    repeatestNumber = numbers[i];
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"число {repeatestNumber} повторилось {maxRepeatsCount} раз подряд, что является наибольшим числом повторений.");
+            Console.ReadKey();
         }
     }
 }
